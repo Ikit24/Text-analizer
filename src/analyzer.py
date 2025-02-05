06384a09b7afb11cd42528d433822663bbf5035c
AttilaSz <szszattila2@gmail.com>
2025-02-05 18:16:11 +0200

    Added basic unttests, added code to be tested

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,1 +1,12 @@
-ÿþ
+class TextAnalyzer:
+    def __init__(self):
+        pass
+    
+    def count_words(self, text):
+        if not text:
+            return 0
+        words = text.split()
+        return len(words)
+    
+    def count_chars(self, text):
+        return len(text)
